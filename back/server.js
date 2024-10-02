7f621c2d3c23e20fb34eaabf471d034c3fc2ea84
FlowXII <zidany.mehdi@gmail.com>
2024-10-02 18:04:48 +0200

    add improvements to design

--- back/server.js
+++ back/server.js
@@ -1,20 +1,34 @@
-// back/server.js
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';  // Import the cors package
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import upcomingTournamentsRoute from './routes/upcomingTournamentsRoute.js';
-import stationViewerRoute from './routes/StationViewerRoute.js';  // Add this line
+import stationViewerRoute from './routes/StationViewerRoute.js';
 
 dotenv.config();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Set up CORS
 app.use(cors());
 
-app.use(express.json());  // Add this line to parse JSON request bodies
+app.use(express.json());
+
+// API routes
 app.use('/api/tournaments', upcomingTournamentsRoute);
-app.use('/api', stationViewerRoute);  // Add this line
+app.use('/api', stationViewerRoute);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
