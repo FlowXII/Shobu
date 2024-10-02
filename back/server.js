62efab7ecbee739ba835b82ed9db5643ec67625d
FlowXII <zidany.mehdi@gmail.com>
2024-10-02 18:09:31 +0200

    add improvements to design

--- back/server.js
+++ back/server.js
@@ -31,7 +31,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
