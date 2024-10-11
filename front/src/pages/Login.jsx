5f02e81d822f65f4da5e69489a180d47df0a67bc
FlowXII <zidany.mehdi@gmail.com>
2024-10-11 22:55:08 +0200

    fix login bug on deploy

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000
 
 function Login() {
   const handleLogin = () => {
-    window.location.href = `${API_BASE_URL}/auth/startgg`;
+    window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/startgg`;
   };
 
   return (
